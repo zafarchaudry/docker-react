f2e26517acde2e2ae19b4f2bf6b8ed53f51d9ddc
zafar <zafar@gmail.com>
2020-09-13 13:23:16 -0700

    feature branch push

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and Cra$$$ing to reload.
+          Edit <code>src/App.js</code> and featureBranch to reload.
         </p>
         <a
           className="App-link"
